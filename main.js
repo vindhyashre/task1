b10ca433bb1f477453ba9ddeee425164e9a96dcf
Vindhya Shree <140383596+vindhyashre@users.noreply.github.com>
2024-10-22 17:33:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function ($) {
+    "use strict";
+
+    // Scroll to sections
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+});
